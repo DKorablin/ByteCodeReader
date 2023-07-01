1d9721e0e8b3c0bdda412351ac3a85f9e10eeb41
DKorablin <AlphaOmega@narod.ru>
2023-07-01 18:22:38 +0300

    YML: Testing custom semVer

--- ByteCodeReader/Properties/AssemblyInfo.cs
+++ ByteCodeReader/Properties/AssemblyInfo.cs
@@ -2,11 +2,8 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-
 [assembly: Guid("103257cc-f3a0-4d18-b9a4-d16f3525e8ee")]
 
 [assembly: System.CLSCompliant(false)]
