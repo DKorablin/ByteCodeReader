f116813a3db15f926a3051077603bd5b510796cd
DKorablin <AlphaOmega@narod.ru>
2023-07-03 18:06:39 +0300

    AssemblyInfo: Trying to unify AssemblyInfo attributes

--- ByteCodeReader/Properties/AssemblyInfo.cs
+++ ByteCodeReader/Properties/AssemblyInfo.cs
@@ -1,9 +1,20 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyDescription("")]
-
 [assembly: ComVisible(false)]
 [assembly: Guid("103257cc-f3a0-4d18-b9a4-d16f3525e8ee")]
 
 [assembly: System.CLSCompliant(false)]
+
+[assembly: AssemblyCompany("Danila Korablin")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2023")]
+[assembly: AssemblyProduct("JVM ClassFile reader")]
+[assembly: AssemblyTitle("Byte Code Reader")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
